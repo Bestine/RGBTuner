e3c21ae2d16cc07805856e3d980cf66860a69eb5
Bestine <bestineokinda@gmail.com>
2022-07-30 21:35:06 +0300

    Created the Frame

--- src/RGBTuner/RGBSlider.java
+++ src/RGBTuner/RGBSlider.java
@@ -6,8 +6,32 @@ import javax.swing.event.*;
 
 public class RGBSlider implements ChangeListener{
 	
+	JSlider redSlider;
+	JSlider greenSlider;
+	JSlider blueSlider;
+	
 	RGBSlider() {
-		System.out.println("Hello world");
+		
+		redSlider = new JSlider(0, 255, 0);
+		greenSlider = new JSlider(0, 255, 0);
+		blueSlider = new JSlider(0, 255, 0);
+		
+		JPanel redPanel = new JPanel();
+		JPanel greenPanel = new JPanel();
+		JPanel bluePanel = new JPanel();
+		
+		JFrame frame = new JFrame();
+		frame.setLayout(null);
+		frame.setTitle("RGB tuner");
+		frame.setSize(900, 700);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		
+		
+		
 	}
 
 	@Override
